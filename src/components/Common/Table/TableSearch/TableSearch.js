d28860bd14b2761864d6a6af3e9a67168cad68eb
Quang Bui <quangbui@cvpcorp.com>
2021-01-14 15:25:21 -0500

    code smells

--- src/components/Common/Table/TableSearch/TableSearch.js
+++ src/components/Common/Table/TableSearch/TableSearch.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./TableSearch.css";
-import { Button,Form, Label } from "@trussworks/react-uswds";
+import { Button, Form, Label } from "@trussworks/react-uswds";
 const TableSearch = ({ setGlobalFilter, title }) => {
   const [searchState, setSearchState] = useState("");
 
@@ -10,35 +10,35 @@ const TableSearch = ({ setGlobalFilter, title }) => {
 
   const updateSearchHandler = () => {
     setGlobalFilter(searchState || undefined);
-  }
+  };
   return (
     <div>
       <Label className="filterLabel" htmlFor="input-search">
         Filter by keyword:
       </Label>
       <div>
-      <Form children={
-        <input id="input-search"
-        name="input-search"
-          className="searchBox"
-          type="text"
-          value={searchState}
-          placeholder={"Keyword"}
-          onChange={(e) => {
-            searchHandler(e.target.value);
-          }}
-          
-        />}
-        
-       >
-      </Form>
-      <Button
-        className="filterBTN"
-        id="input-button-search"
-        onClick={updateSearchHandler}
-      >
-        Filter
-      </Button>
+        <Form
+          children={
+            <input
+              id="input-search"
+              name="input-search"
+              className="searchBox"
+              type="text"
+              value={searchState}
+              placeholder={"Keyword"}
+              onChange={(e) => {
+                searchHandler(e.target.value);
+              }}
+            />
+          }
+        ></Form>
+        <Button
+          className="filterBTN"
+          id="input-button-search"
+          onClick={updateSearchHandler}
+        >
+          Filter
+        </Button>
       </div>
     </div>
   );
