685f84c20cf6f629125e4474aad989002b70b271
Joshua Mackey <Joshua.Mackey@aplaceformom.com>
2021-05-06 16:18:35 -0400

    Login page ready for testing.

--- src/utils/api/easeyAuthApi.js
+++ src/utils/api/easeyAuthApi.js
@@ -1,23 +1,7 @@
 import axios from "axios";
-// import { handleError } from "./apiUtils";
 import config from "../../config";
 
 export async function authenticate(data_payload) {
-  // return axios
-  //   .post(`${config.services.authApi.uri}/authentication/authenticate`, data_payload)
-  //   .then(response => {
-  //       const { data } = response;
-  //       sessionStorage.setItem('cdx_user', JSON.stringify(data));
-  //       window.location.reload();
-  //   })
-  //   .catch(handleError);
-
-    // return await axios({
-    //     method: 'POST',
-    //     url: `${config.services.authApi.uri}/authentication/authenticate`,
-    //     data: data_payload
-    // }).then(data_response => data_response.data).catch(e => e);
-
     try {
         return await axios({
             method: 'POST',
